9f981c9dfb413f5101a7cd35c5ac11c5dc811e9b
Fede <devnetcorefdz@gmail.com>
2023-02-10 15:59:24 -0300

    1

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex } from "@react-native-material/core";
+import { Box, Flex, ListItem } from "@react-native-material/core";
 import { Card, DataTable, FAB, List } from "react-native-paper";
 import axios from "axios";
 import { StyleSheet } from "react-native";
@@ -13,7 +13,8 @@ const Menu = () => {
   const [menu, setMenu] = useState([]);  
   const [isLoading, setIsLoading] = useState(false);
   const urlApi = "http://apifdz.somee.com/api/menu/";
-  
+  const [ip, setIP] = useState("");
+
     
     useEffect(() => {
       axios({
@@ -26,47 +27,61 @@ const Menu = () => {
         .catch((error) => {
           console.log(error);
         });
+
+        getData();
+
     }, [setMenu]);
 
-  console.log(menu)
+  
+    const getData = async () => {
+      const res = await axios.get("https://geolocation-db.com/json/");
+      console.log(res.data);
+      setIP(res.data.IPv4);
+    };
     
 
   return (
     <ScrollView>
       <Flex center>
         <Box w={300}>
+          <h4>{ip}</h4>
+
+
           {isLoading ? (
             <>
-            {
-              menu.map((x) => (
-              <Card
-                style={{
-                  marginBottom: "1rem",
-                  //width: "30rem",
-                }}
-                key={x.id}
-              >
-                <Card.Content>
-                  <p>
-                    <Icon path={mdiNoodles} size={1} />
-                    {x.entradaMenu}
-                  </p>
-                  <p>
-                    <Icon path={mdiPasta} size={1} />
-                    {x.principalMenu}
-                  </p>
-                  <p>
-                    <Icon path={mdiIceCream} size={1} />
-                    {x.postreMenu}
-                  </p>
-                </Card.Content>
-              </Card>
+              {menu.map((x) => (
+                <Card
+                  style={{
+                    marginBottom: "1rem",
+                    //width: "30rem",
+                  }}
+                  key={x.id}
+                >
+                  <Card.Content>
+                    <ListItem title={x.diaMenu.toUpperCase()} />
+
+                    <ListItem
+                      title={x.entradaMenu}
+                      leading={<Icon path={mdiNoodles} size={1} />}
+                    />
+
+                    <ListItem
+                      title={x.principalMenu}
+                      leading={<Icon path={mdiPasta} size={1} />}
+                    />
+
+                    <ListItem
+                      title={x.postreMenu}
+                      leading={<Icon path={mdiIceCream} size={1} />}
+                    />
+                  </Card.Content>
+                </Card>
               ))}
             </>
           ) : (
-            <Flex center top={100}>              
-              <FAB                
-                icon={(props) => <Icon name="plus" {...props} />}                
+            <Flex center top={100}>
+              <FAB
+                icon={(props) => <Icon name="plus" {...props} />}
                 loading={true}
               />
             </Flex>
